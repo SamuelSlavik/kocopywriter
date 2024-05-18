7de742586539999d79b89dffb227f7970ce6b033
SamuelSlavik <samuel.slavik@protonmail.com>
2024-05-18 12:25:58 +0200

    Fixed deployment db url

--- backend/core/config.py
+++ backend/core/config.py
@@ -5,7 +5,7 @@ class Config():
     def __init__(self):
         self.BASEDIR = os.path.abspath(os.path.dirname(__file__))
         self.TOKEN_EXPIRATION = 120 # minutes
-        self.SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://default:Qshx82gMEYNJ@ep-young-hill-a2xr36fw-pooler.eu-central-1.aws.neon.tech:5432/verceldb") # Change tou your db
+        self.SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://default:Qshx82gMEYNJ@ep-young-hill-a2xr36fw-pooler.eu-central-1.aws.neon.tech:5432/verceldb?sslmode=require") # Change tou your db
         self.FRONTEND_URL = os.getenv("FRONTEND_URL", "https://kocopywriter-frontend.vercel.app/") # Change to your localhost
         self.IMAGES_DIR = os.path.join("images", "")
         self.BRANDS_IMAGES_DIR = os.path.join("images", "brands")
