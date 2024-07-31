712b2c7ef0d53411e32ca5c641f58a1751eeb9ac
SamuelSlavik <samuel.slavik@protonmail.com>
2024-07-31 14:55:48 +0200

    Add Image slider

--- frontend/src/pages/projects/ImageSlider.vue
+++ frontend/src/pages/projects/ImageSlider.vue
@@ -24,18 +24,20 @@ const prev = () => {
 <template>
   <div class="image-slider">
     <div class="image-slider__overlay" @click="$emit('close')"></div>
-    <button class="image-slider__close" @click="$emit('close')">
-      <Close :size="32"/>
-    </button>
-    <button class="image-slider__nav image-slider__nav--prev" @click="prev">
-      <ArrowLeft :size="32"/>
-    </button>
-    <div class="image-slider__content">
-      <img :src="currentImage" alt="Zoomed project image" class="image-slider__image" />
+    <div class="image-slider__controls">
+      <button class="image-slider__close" @click="$emit('close')">
+        <Close :size="32"/>
+      </button>
+      <button class="image-slider__nav image-slider__nav--prev" @click="prev">
+        <ArrowLeft :size="32"/>
+      </button>
+      <div class="image-slider__content">
+        <img :src="currentImage" alt="Zoomed project image" class="image-slider__image" />
+      </div>
+      <button class="image-slider__nav image-slider__nav--next" @click="next">
+        <ArrowRight :size="32"/>
+      </button>
     </div>
-    <button class="image-slider__nav image-slider__nav--next" @click="next">
-      <ArrowRight :size="32"/>
-    </button>
   </div>
 </template>
 
@@ -62,11 +64,19 @@ const prev = () => {
   z-index: 20001;
 }
 
-.image-slider__content {
+.image-slider__controls {
   position: relative;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 20002;
+}
+
+.image-slider__content {
   max-width: calc(100% - 20% - 4rem);
   max-height: 90%;
-  z-index: 20002;
 }
 
 .image-slider__image {
@@ -83,7 +93,6 @@ const prev = () => {
   color: var(--background);
   font-size: 2rem;
   cursor: pointer;
-  z-index: 20003;
   &:hover {
     color: var(--orange);
   }
